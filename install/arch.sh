8c0235931f302c7ab0f79df3c0a6a187728b15ee
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2022-04-11 09:15:34 -0300

    update asdf install

--- install/arch.sh
+++ install/arch.sh
@@ -12,4 +12,4 @@ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.
 
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
-git clone https://aur.archlinux.org/asdf-vm.git && cd asdf-vm && makepkg -si
+git clone https://aur.archlinux.org/asdf-vm.git && cd "$(basename "$_" .git)" && makepkg -si
