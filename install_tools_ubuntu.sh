0f34e1343f8f500f3238ed1dbe8974f499e7eda4
Rian Negreiros Dos Santos <72347639+RianNegreiros@users.noreply.github.com>
2024-02-17 09:00:10 -0300

    add script `install_tools_ubuntu.sh`

---
+++ install_tools_ubuntu.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Update package lists
+sudo apt-get update
+
+# Install ZSH
+sudo apt-get install -y zsh
+
+# Install Oh My Zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Install ASDF VM
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.8.0
+
+# Install Powerlevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+# Install ZSH Syntax Highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+# Install ZSH Autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+# Source ZSH to take changes into effect
+source ~/.zshrc
