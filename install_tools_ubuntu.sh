de1e404e3f638f8c56d1f4f4504eee436165b6f5
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2025-04-26 11:12:47 -0300

    Improve Ubuntu development tools installation script

--- install_tools_ubuntu.sh
+++ install_tools_ubuntu.sh
@@ -1,25 +1,69 @@
 #!/bin/bash
+set -e  # Exit on error
+
+echo "Starting installation of development tools..."
 
 # Update package lists
+echo "Updating package lists..."
 sudo apt-get update
 
-# Install ZSH
-sudo apt-get install -y zsh
+# Install required dependencies
+echo "Installing dependencies..."
+sudo apt-get install -y zsh curl git
 
 # Install Oh My Zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+echo "Installing Oh My Zsh..."
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+else
+    echo "Oh My Zsh is already installed, skipping..."
+fi
 
-# Install ASDF VM
-git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.8.0
+# Install ASDF VM (latest version)
+echo "Installing ASDF VM..."
+if [ ! -d "$HOME/.asdf" ]; then
+    git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.13.1
+else
+    echo "ASDF VM is already installed, skipping..."
+fi
+
+# Add ASDF to shell configuration
+echo "Configuring ASDF in shell..."
+ASDF_CONFIG='. "$HOME/.asdf/asdf.sh"
+. "$HOME/.asdf/completions/asdf.bash"'
+grep -qF "$ASDF_CONFIG" ~/.zshrc || echo "$ASDF_CONFIG" >> ~/.zshrc
 
 # Install Powerlevel10k
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+echo "Installing Powerlevel10k theme..."
+if [ ! -d "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k" ]; then
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+else
+    echo "Powerlevel10k is already installed, skipping..."
+fi
+
+# Set Powerlevel10k as the ZSH theme
+sed -i 's/ZSH_THEME=".*"/ZSH_THEME="powerlevel10k\/powerlevel10k"/' ~/.zshrc
 
 # Install ZSH Syntax Highlighting
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+echo "Installing ZSH Syntax Highlighting..."
+if [ ! -d "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting" ]; then
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+else
+    echo "ZSH Syntax Highlighting is already installed, skipping..."
+fi
 
 # Install ZSH Autosuggestions
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+echo "Installing ZSH Autosuggestions..."
+if [ ! -d "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions" ]; then
+    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+else
+    echo "ZSH Autosuggestions is already installed, skipping..."
+fi
 
-# Source ZSH to take changes into effect
-source ~/.zshrc
+# Configure plugins in .zshrc
+PLUGINS_LINE='plugins=(git zsh-syntax-highlighting zsh-autosuggestions)'
+sed -i "s/plugins=(.*)/plugins=(git zsh-syntax-highlighting zsh-autosuggestions)/" ~/.zshrc
+
+echo "Installation complete!"
+echo "Please restart your terminal or run 'zsh' to apply changes."
+echo "When you first start ZSH with Powerlevel10k, a configuration wizard will appear."
